ee90cd4ed0b7625420f5d909ef78cf1670030d51
Владислав Рыбачук <vlad1k-o@yandex.ru>
2023-03-04 22:10:46 +0300

    added permissions

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,31 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CommentViewSet, ReviewViewSet,
+                    CategoryViewSet, GenreViewSet, TitleViewSet)
+
+
+
+api_name = 'api'
+
+router_v1 = DefaultRouter()
+
+router_v1.register(
+    r'^titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router_v1.register(
+    r'^titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_v1.register(r'genres', GenreViewSet, basename='genre')
+router_v1.register(r'categories', CategoryViewSet, basename='category')
+router_v1.register(r'title', TitleViewSet, basename='title')
+
+urlpatterns = [
+    path('api/v1/', include(router_v1.urls)),
+    path('v1/auth/', ()),
+    path('v1/users/', ())
+]

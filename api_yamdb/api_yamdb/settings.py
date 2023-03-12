f36644b1a0cbda9c016b32699f500582e00e064b
Lerusha-junior <lerush-jukova@yandex.ru>
2023-03-12 13:42:35 +0400

    Resolved merge conflict by incorporating both suggestions.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-
 AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'

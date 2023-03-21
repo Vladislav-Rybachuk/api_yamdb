48a9ff59914404bcd6416773b46e2cdf74db2db9
Владислав Рыбачук <vlad1k-o@yandex.ru>
2023-03-22 00:19:31 +0300

    bugfix

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,26 +1,35 @@
 import csv
+import os
 
+from django.conf import settings
 from django.apps import apps
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Creating model objects according the file path specified'
+    help = "Creating model objects according the file path specified"
 
     def add_arguments(self, parser):
-        parser.add_argument('--path', type=str, help="file path")
-        parser.add_argument('--model_name', type=str, help="model name")
         parser.add_argument(
-            '--app_name', type=str,
-            help="django app name that the model is connected to")
+            "--path",
+            type=str,
+            default=os.path.join(settings.BASE_DIR, "static"),
+            help="file path",
+        )
+        parser.add_argument("--model_name", type=str, help="model name")
+        parser.add_argument(
+            "--app_name",
+            type=str,
+            help="django app name that the model is connected to",
+        )
 
     def handle(self, *args, **kwargs):
-        model_name = (kwargs['model_name'])
+        model_name = apps.get_model(kwargs["app_name"], kwargs["model_name"])
         model_name.objects.all().delete()
-        file_path = kwargs['path']
-        _model = apps.get_model(kwargs['app_name'], kwargs['model_name'])
-        with open(file_path, "rt", encoding='utf-8') as csv_file:
-            reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        file_path = kwargs["path"]
+        _model = apps.get_model(kwargs["app_name"], kwargs["model_name"])
+        with open(file_path, "rt", encoding="utf-8") as csv_file:
+            reader = csv.reader(csv_file, delimiter=",", quotechar="|")
             header = next(reader)
             for row in reader:
                 _object_dict = {key: value for key, value in zip(header, row)}
